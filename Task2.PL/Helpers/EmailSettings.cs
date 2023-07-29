b52bcc61c20f1c8769b846b0ddcc429acb55b497
Ebtesam20 <ebtesammahmoud200@gmail.com>
2023-07-29 23:49:27 +0300

    initial commit

---
+++ Task2.PL/Helpers/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Task2.DAL.Models;
+
+namespace Task2.PL.Helpers
+{
+	public static class EmailSettings
+	{
+		public static void SendEmail(Email email)
+		{
+			var Client = new SmtpClient("smtp.gmail.com",587);
+			Client.EnableSsl= true;
+			Client.Credentials = new NetworkCredential("ebtesammahmoud200@gmail.com", "hsfqpcbjrrdtoslt");
+			Client.Send("ebtesammahmoud200@gmail.com", email.To, email.Subject, email.Body);
+
+
+		}
+	}
+}
